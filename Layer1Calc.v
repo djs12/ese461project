adeb55e58b0a05d25da9ccfb24a1f9af461ba5f6
dskelton12 <dskelton12@gmail.com>
2017-12-03 14:30:03 -0600

    Update Layer1Calc.v

--- Layer1Calc.v
+++ Layer1Calc.v
@@ -5,7 +5,7 @@ input  [15:0]  weight;
 input  [159:0] pixels;
 output [159:0] column;
 
-   Mac M1(clk, reset, pixels[15:0], weight, column[15:0]);
+Mac M1(clk, reset, pixels[15:0], weight, column[15:0]);
 Mac M2(clk, reset, pixels[31:16], weight, column[31:16]);
 Mac M3(clk, reset, pixels[47:32], weight, column[47:32]);
 Mac M4(clk, reset, pixels[63:48], weight, column[63:48]);
