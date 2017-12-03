fe9f48af3c78dd7c729c7a3960df036dd7a02848
dskelton12 <dskelton12@gmail.com>
2017-12-03 16:49:23 -0600

    Add files via upload

--- Layer1Calc.v
+++ Layer1Calc.v
@@ -5,15 +5,46 @@ input  [15:0]  weight;
 input  [159:0] pixels;
 output [159:0] column;
 
-Mac M1(clk, reset, pixels[15:0], weight, column[15:0]);
-Mac M2(clk, reset, pixels[31:16], weight, column[31:16]);
-Mac M3(clk, reset, pixels[47:32], weight, column[47:32]);
-Mac M4(clk, reset, pixels[63:48], weight, column[63:48]);
-Mac M5(clk, reset, pixels[79:64], weight, column[79:64]);
-Mac M6(clk, reset, pixels[95:80], weight, column[95:80]);
-Mac M7(clk, reset, pixels[111:96], weight, column[111:96]);
-Mac M8(clk, reset, pixels[127:112], weight, column[127:112]);
-Mac M9(clk, reset, pixels[143:128], weight, column[143:128]);
-Mac M10(clk, reset, pixels[159:144], weight, column[159:144]);
+reg [159:0] add_in;
 
-endmodule
+Mac M1(clk, reset, pixels[15:0], weight, add_in[15:0], column[15:0]);
+Mac M2(clk, reset, pixels[31:16], weight, add_in[31:16], column[31:16]);
+Mac M3(clk, reset, pixels[47:32], weight, add_in[47:32], column[47:32]);
+Mac M4(clk, reset, pixels[63:48], weight, add_in[63:48], column[63:48],);
+Mac M5(clk, reset, pixels[79:64], weight, add_in[79:64], column[79:64]);
+Mac M6(clk, reset, pixels[95:80], weight, add_in[95:80], column[95:80]);
+Mac M7(clk, reset, pixels[111:96], weight, add_in[111:96], column[111:96]);
+Mac M8(clk, reset, pixels[127:112], weight, add_in[127:112], column[127:112]);
+Mac M9(clk, reset, pixels[143:128], weight, add_in[143:128], column[143:128]);
+Mac M10(clk, reset, pixels[159:144], weight, add_in[159:144], column[159:144]);
+
+always @ (reset)
+begin
+   case(reset)
+   0: begin
+      add_in[15:0] = column[15:0];
+      add_in[31:16] = column[31:16];
+      add_in[47:32] = column[47:32];
+      add_in[63:48] = column[63:48];
+      add_in[79:64] = column[79:64];
+      add_in[95:80] = column[95:80];
+      add_in[111:96] = column[111:96];
+      add_in[127:112] = column[127:112];
+      add_in[143:128] = column[143:128];
+      add_in[159:144] = column[159:144];
+      end
+   1: begin
+      add_in[15:0] = 0;
+      add_in[31:16] = 0;
+      add_in[47:32] = 0;
+      add_in[63:48] = 0;
+      add_in[79:64] = 0;
+      add_in[95:80] = 0;
+      add_in[111:96] = 0;
+      add_in[127:112] = 0;
+      add_in[143:128] = 0;
+      add_in[159:144] = 0;
+      end
+end
+endmodule   
+
