169b83f194b1a46dda7ef4c0c5fa47d0b0b94952
ameliama <32748520+ameliama@users.noreply.github.com>
2017-12-04 15:40:25 -0600

    Update controller.v

--- controller.v
+++ controller.v
@@ -4,13 +4,13 @@ module controller(
     input clk,
     input reset,
     
-    output reg MAC_layer1_mux, //0: add to partial sum. 1: add to 0 (clearing)
+    output reg MAC_reset, //0: add to partial sum. 1: add to 0 (clearing)
     
     output reg reg_holder_in, // write enable of reg holder
-    output reg reg_holder_mux_in, //0: load from MACs, 160 bits. 1: load from LUT, 16 bits, address provided
-    output reg reg_holder_out, // output enable of reg holder
-    output reg reg_holder_mux_out, //0: output to LUT, 16 bits. 1: output to Multiplier, 16 bits. Address provided  
+    output reg reg_holder_mux, //0: load from MACs, 160 bits. 1: load from LUT, 16 bits, address provided
     output reg [3:0] reg_holder_addr,
+    
+    output reg LUT_mux, // 0: load from reg_holder, 1: load from GSRAM
     
     output reg weight2_loadNextElement,
     output reg weight2_loadNextRow,
@@ -18,18 +18,16 @@ module controller(
     output reg [3:0] GSRAM_addr_row, // 0~9
     output reg [3:0] GSRAM_addr_col, // 0~9
     output reg GSRAM_in, 
-    output reg GSRAM_in_mux, // 0->going to the adder, 1->going to the LUT
-    output reg GSRAM_out,
-    output reg GSRAM_out_mux // 0->going to the adder, 1->going to the LUT
+    output reg GSRAM_mux // 0->going to the adder, 1->going to the LUT
     );
 
 reg [9:0] count_layer1_784D, count_layer1_784Q;
 reg [7:0] count_layer1_200D, count_layer1_200Q;
-    reg [3:0] count_10D, count_10Q, count_10_2D, count_10_2Q;
+reg [3:0] count_10D, count_10Q, count_10_2D, count_10_2Q;
 
 reg [2:0] currentState, nextState;
 
-parameter IDLE=0, REG=1, REG_OUT_TO_LUT=2, REG_IN_TO_REG=3, REG_OUT_TO_MAC=4, GSRAM_OUT_TO_LUT=5;
+parameter IDLE=0, REG=1, REG_TO_LUT=2, LUT_TO_REG=3, REG_TO_MAC=4, GSRAM_TO_LUT=5, LUT_TO_GSRAM=6;
 
 always @ * begin 
     //default values
@@ -38,20 +36,17 @@ always @ * begin
     count_layer1_200D = count_layer1_200Q;
     count_10D = count_10Q;
     count_10_2D = count_10_2Q;
-    MAC_layer1_mux = 0;
+    MAC_reset = 0;
     reg_holder_in = 0;
-    reg_holder_mux_in = 0;
-    reg_holder_out = 0;
-    reg_holder_mux_out = 0;
+    reg_holder_mux = 0;
     reg_holder_addr = 0;
+    LUT_mux = 0;
     weight2_loadNextElement = 0;
     weight2_loadNextRow = 0;
     GSRAM_addr_row = 0;
     GSRAM_addr_col = 0;
-    GSRAM_in = 0;
-    GSRAM_in_mux = 0;
-    GSRAM_out = 0;
-    GSRAM_out_mux = 0; 
+    GSRAM_in = 0; 
+    GSRAM_mux = 0;
     
     if (count_layer1_200Q == 200) begin //if work of layer 1 is done, block layer 1 (this is not power efficient, maybe other ways?)
         count_layer1_784D = 0;
@@ -67,52 +62,47 @@ always @ * begin
     end
     
     REG: begin
-        MAC_layer1_mux = 1;
+        MAC_reset = 1;
         reg_holder_in = 1;
-        reg_holder_mux_in = 0; //load from MACs
+        reg_holder_mux = 0; //load from MACs
         count_10D = 0;
-        nextState = REG_OUT_TO_LUT;
+        nextState = REG_TO_LUT;
     end
     
-    REG_OUT_TO_LUT: begin
-        reg_holder_out = 1;
-        reg_holder_mux_out = 0; //output to LUT
+    REG_TO_LUT: begin
+        LUT_mux = 0; // load from reg_holder
         reg_holder_addr = count_10Q;
-        nextState = REG_IN_TO_REG;
+        nextState = LUT_TO_REG;
     end
     
-    REG_IN_TO_REG: begin
+    LUT_TO_REG: begin
         reg_holder_in = 1;
-        reg_holder_mux_in = 1; // load from LUT
+        reg_holder_mux = 1; // load from LUT
         reg_holder_addr = count_10Q;
         if (count_10Q==9) begin
             count_10D = 0;
             weight2_loadNextRow = 1;
             weight2_loadNextElement = 1;
-            nextState = REG_OUT_TO_MAC;
+            nextState = REG_TO_MAC;
         end else begin
             count_10D = count_10Q + 1;
-            nextState = REG_OUT_TO_LUT;
+            nextState = REG_TO_LUT;
         end
     end
     
     //assuming mult + add + store into 10x10 SRAM takes 1 clk cycle altogether
-    REG_OUT_TO_MAC: begin
+    REG_TO_MAC: begin
+        GSRAM_addr_row = count_10Q;
+        GSRAM_addr_col = count_10_2Q;
+        reg_holder_addr = count_10Q;
         if (count_10_2Q == 9 && count_10Q == 9) begin
             count_10D = 0;
             count_10_2D = 0;
-            nextState = GSRAM_OUT_TO_LUT;
+            nextState = GSRAM_TO_LUT;
         end else begin
             count_10D = count_10Q + 1;
-            reg_holder_out = 1;
-            reg_holder_mux_out = 1;
-            reg_holder_addr = count_10Q;
             GSRAM_in = 1;
-            GSRAM_in_mux = 0;
-            GSRAM_out = 1;
-            GSRAM_out_mux = 0;
-            GSRAM_addr_row = count_10Q;
-            GSRAM_addr_col = count_10_2Q;
+            GSRAM_mux = 0; // load from adder
             if (count_10Q == 9) begin
                 count_10D = 0;
                 count_10_2D = count_10_2Q + 1;
@@ -121,26 +111,31 @@ always @ * begin
         end
     end
     
-    GSRAM_OUT_TO_LUT: begin
-        count_10D = count_10Q + 1;
-        GSRAM_out = 1;
-        GSRAM_out_mux = 1;
+    GSRAM_TO_LUT: begin
         GSRAM_addr_row = count_10Q;
         GSRAM_addr_col = count_10_2Q;
-        nextState = GSRAM_OUT_TO_LUT_MID;
+        LUT_mux = 1; //load from GSRAM
+        nextState = LUT_TO_GSRAM;
     end
         
-    LUT_IN_TO_GSRAM: begin
+    LUT_TO_GSRAM: begin
+        GSRAM_in = 1;
+        GSRAM_mux = 1; // load from GSRAM
+        GSRAM_addr_row = count_10Q;
+        GSRAM_addr_col = count_10_2Q;
         if (count_10_2Q == 9 && count_10Q == 9) begin
             count_10D = 0;
             count_10_2D = 0;
-            nextState = GSRAM_OUT_TO_LUT_LAST;
+            nextState = IDLE;
         end else begin
-            GSRAM_out = 1;
-            GSRAM_out_mux = 1;
-            GSRAM_in = 1;
-            GSRAM_in_mux = 1;
-            
+            nextState = GSRAM_TO_LUT;
+            if (count_10Q==9) begin
+                count_10D = 0;
+                count_10_2D = count_10_2Q + 1;
+            end else begin
+                count_10D = count_10Q + 1;
+                nextState = GSRAM_TO_LUT;
+            end
         end
     end
     
@@ -166,4 +161,3 @@ always @ (posedge clk) begin
 end
     
 endmodule
-
