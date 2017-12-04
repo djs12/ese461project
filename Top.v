9688bd95b24364680c0d351c7e498a98789b8fe4
djs12 <danielsullivan787@gmail.com>
2017-12-04 16:16:33 -0600

    Add files via upload

--- Top.v
+++ Top.v
@@ -1,17 +1,90 @@
-module Top
-(
-	input [15:0] data,
-	input [9:0] addr,
-	input we, 
-	input clk,
-	output [15:0] q
+module Top(
+clk,
+reset,
+weight1
+
 );
 
-//module Layer1Calc(clk, reset, pixels[159:0], weight, column);
-inputSRAM S1(.data(data), /*.addr(addr), .we(we), .clk(clk), */.q(l1Inputs));
-Layer1Calc L1(clk, layer1Reset, l1Inputs, l1Weight, layer1Out);
-Sigmoid()
+/*
+*************************************************************************
+Inputs
+*************************************************************************
+*/
+input clk;
+input reset;
+input [15:0] weight1;
 
-	
+
+/*
+*************************************************************************
+Wires and Connections
+*************************************************************************
+*/
+
+//Layer 1 connections
+wire [159:0] pixels;
+wire [159:0] column;
+
+
+//Input Vector SRAM connections
+wire [159:0] q_w1;
+
+//Weight 2 row vector SRAM connections
+wire [3:0] addr;
+wire [15:0] q_w2;
+
+//gSRAM connections
+wire we;
+wire [3:0] row;
+wire [3:0] col;
+wire [15:0] wdata;
+wire [15:0] rdata;
+
+//sigmoid connections
+wire [15:0] sig_in;
+wire [15:0] sig_out;
+
+
+/*
+*************************************************************************
+Instantiation of Modules
+*************************************************************************
+*/
+
+Layer1Calc STAGE_1(
+.clk(clk),
+.reset(reset),
+.pixels(pixels),
+.weight(weight1),
+.column(column)
+);
+
+inputSRAM INPUTS(
+.clk(clk),
+.q(q_w1)
+);
+
+w2SRAM WEIGHT_2(
+.clk(clk),
+.addr(addr),
+.q(q_w2)
+);
+
+gSRAM ANSWER(
+.clk(clk),
+.we(we),
+.row(row),
+.col(col),
+.wdata(wdata),
+.rdata(rdata)
+);
+
+sigmoid SIGMOID(
+.clk(clk),
+.sig_in(sig_in),
+.sig_out(sig_out)
+);
+
+
+
 endmodule
-
