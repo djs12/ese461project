6a3a8b364db495b0cad4b8e803968fa9e797e5ac
dskelton12 <dskelton12@gmail.com>
2017-12-04 18:14:48 -0600

    Add files via upload

--- Top.v
+++ Top.v
@@ -1,3 +1,9 @@
+
+//module Layer1Calc(clk, reset, pixels[159:0], weight, column);
+//inputSRAM S1(.data(data), /*.addr(addr), .we(we), .clk(clk), */.q(l1Inputs));
+//Layer1Calc L1(clk, layer1Reset, l1Inputs, l1Weight, layer1Out);
+//Sigmoid()
+
 module Top(
 clk,
 reset,
@@ -44,6 +50,13 @@ wire [15:0] rdata;
 wire [15:0] sig_in;
 wire [15:0] sig_out;
 
+//controller 
+wire l1reset;
+wire routeDataRegWrEn;
+wire routeDataRegWrSel;
+wire routeDataOutMuxSel;
+wire [4:0] routeDataRegAddr;
+
 
 /*
 *************************************************************************
@@ -53,22 +66,54 @@ Instantiation of Modules
 
 Layer1Calc STAGE_1(
 .clk(clk),
-.reset(reset),
+.reset(l1reset), //verify this is the correct reset
 .pixels(pixels),
 .weight(weight1),
 .column(column)
 );
+
+
+controller CNTRL(
+.clk(clk),
+.reset(reset),
+.MAC_reset(l1reset), //0: add to partial sum. 1: add to 0 (clearing)
+.reg_holder_in(routeDataRegWrEn),
+.reg_holder_mux(routeDataRegWrSel),
+.reg_holder_addr(routeDataRegAddr),
+.LUT_mux(routeDataOutMuxSel),
+.weight2_addr(addr),
+.weight2_loadNextRow(////),
+.GSRAM_addr_row(row),
+.GSRAM_addr_col(col),
+.GSRAM_in(we),
+.GSRAM_mux(///////)
+);
+
+RouteData ROUTEDATA(
+.clk(clk),
+.M1Result(column),
+.SigFeedback(sig_out),
+.SramData(rdata),
+.RegLoadEn(routeDataRegWrEn),
+.RegLoadSel(routeDataRegWrSel),
+.Addr(routeDataRegAddr),
+.DataOutSel(routeDataOutMuxSel),
+.DataOut(sig_in)
+);
+
 
 inputSRAM INPUTS(
 .clk(clk),
 .q(q_w1)
 );
 
+
 w2SRAM WEIGHT_2(
 .clk(clk),
 .addr(addr),
 .q(q_w2)
 );
+
 
 gSRAM ANSWER(
 .clk(clk),
@@ -79,6 +124,7 @@ gSRAM ANSWER(
 .rdata(rdata)
 );
 
+
 sigmoid SIGMOID(
 .clk(clk),
 .sig_in(sig_in),
@@ -86,5 +132,6 @@ sigmoid SIGMOID(
 );
 
 
-
 endmodule
+
+
