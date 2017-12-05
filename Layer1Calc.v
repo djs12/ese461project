009c6345fad7bdced7bf844693ce717f91bff897
dskelton12 <dskelton12@gmail.com>
2017-12-04 19:27:41 -0600

    Add files via upload

--- Layer1Calc.v
+++ Layer1Calc.v
@@ -1,4 +1,4 @@
-module Layer1Calc(clk, reset, pixels[159:0], weight, column);
+module Layer1Calc(clk, reset, pixels, weight, column);
 
 input clk, reset;
 input  [15:0]  weight;
@@ -10,7 +10,7 @@ reg [159:0] add_in;
 Mac M1(clk, reset, pixels[15:0], weight, add_in[15:0], column[15:0]);
 Mac M2(clk, reset, pixels[31:16], weight, add_in[31:16], column[31:16]);
 Mac M3(clk, reset, pixels[47:32], weight, add_in[47:32], column[47:32]);
-Mac M4(clk, reset, pixels[63:48], weight, add_in[63:48], column[63:48],);
+Mac M4(clk, reset, pixels[63:48], weight, add_in[63:48], column[63:48]);
 Mac M5(clk, reset, pixels[79:64], weight, add_in[79:64], column[79:64]);
 Mac M6(clk, reset, pixels[95:80], weight, add_in[95:80], column[95:80]);
 Mac M7(clk, reset, pixels[111:96], weight, add_in[111:96], column[111:96]);
@@ -45,6 +45,8 @@ begin
       add_in[143:128] = 0;
       add_in[159:144] = 0;
       end
+   endcase
 end
+
 endmodule   
 
